dd186e938a7d852c3be75b25a61d326592c757d7
Evgeny Umansky <euguman@gmail.com>
2021-01-09 16:50:42 +0200

    fillMediaItems is finished

--- src/org/bgu/ise/ddb/items/ItemsController.java
+++ src/org/bgu/ise/ddb/items/ItemsController.java
@@ -18,9 +18,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mongodb.MongoClient;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 
 import java.sql.*;
 
@@ -39,18 +41,42 @@ public class ItemsController extends ParentController {
 	public void fillMediaItems(HttpServletResponse response){
 		System.out.println("was here");
 		//:TODO your implementation
+		// get the oracle driver
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection connection = DriverManager.getConnection(
-	                "jdbc:oracle:thin:@132.72.65.216:1521:oracle", "mohsenab", "abcd");
-			Statement statement = connection.createStatement();
-			ResultSet result = statement.executeQuery("SELECT * FROM MediaItems");
-			
-			MongoClient mongoClient = new MongoClient("localhost", 27017);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		// try-with-resources
+		try(
+				Connection connection = DriverManager.getConnection(
+						"jdbc:oracle:thin:@132.72.65.216:1521:oracle", "mohsenab", "abcd"); // open connection to oracle
+				Statement statement = connection.createStatement();
+				ResultSet result = statement.executeQuery("SELECT * FROM MediaItems"); // get results of select query
+				MongoClient mongoClient = new MongoClient("localhost", 27017);// open mongodb connection
+			){
 			DB db = mongoClient.getDB("ProjectATDB");
 			DBCollection collection = db.getCollection("MediaItems");
-			int x = 0;
-			
+			// add the data from result to mongo
+			while(result.next()) {
+				// get title and year from oracle result
+				String title = result.getString("TITLE");
+				int year = Integer.parseInt(result.getString("PROD_YEAR"));
+				
+				// check if the data is already exists in mongo 
+				DBObject isExistQuery = new BasicDBObject("Title", title);
+				DBCursor isExistResult = collection.find(isExistQuery);
+				if(isExistResult.size() > 0) {
+					continue;
+				}
+				
+				// add the data to mongo
+				BasicDBObject insertionDocument = new BasicDBObject();
+				insertionDocument.put("Title", title);
+				insertionDocument.put("Prod_Year", year);
+				collection.insert(insertionDocument);
+			}
 		}
 		catch(Exception e) {
 			e.printStackTrace();
@@ -72,8 +98,22 @@ public class ItemsController extends ParentController {
 	public void fillMediaItemsFromUrl(@RequestParam("url")    String urladdress,
 			HttpServletResponse response) throws IOException{
 		System.out.println(urladdress);
-		
 		//:TODO your implementation
+		if(urladdress == null || urladdress.isEmpty()) {
+			HttpStatus status = HttpStatus.CONFLICT;
+			response.setStatus(status.value());
+			return;
+		}
+		
+	    try {
+	        URL rowdata = new URL(url);
+	        URLConnection data = rowdata.openConnection();
+	        Scanner input = new Scanner(data.getInputStream());
+	    }
+	    catch(Exception e) {
+	    	e.printStackTrace();
+	    }
+		
 		HttpStatus status = HttpStatus.OK;
 		response.setStatus(status.value());
 	}
